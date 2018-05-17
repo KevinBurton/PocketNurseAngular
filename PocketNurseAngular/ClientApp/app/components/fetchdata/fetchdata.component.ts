f4f12c21ab41213b01be349a9316cac449f3e567
Ronald Kevin Burton <rkevinburton@charter.net>
2018-05-16 23:08:40 -0500

    Preliminary file type and Excel spreadsheet read.

--- PocketNurseAngular/ClientApp/app/components/fetchdata/fetchdata.component.ts
+++ PocketNurseAngular/ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import * as XLSX from 'xlsx';
+import * as fileType from 'file-type';
 
 @Component({
     selector: 'fetchdata',
@@ -8,7 +9,7 @@ import * as XLSX from 'xlsx';
     styleUrls: [ './fetchdata.component.css' ]
 })
 export class FetchDataComponent implements OnInit {
-
+    content: ArrayBuffer
     fileTypes: string[] = [
         'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     ];
@@ -27,6 +28,7 @@ export class FetchDataComponent implements OnInit {
         console.log(input.value);
 
         var curFiles = input.files;
+
         if (curFiles === null || curFiles.length === 0) {
             var para = document.createElement('p');
             para.textContent = 'No files currently selected for upload';
@@ -34,16 +36,38 @@ export class FetchDataComponent implements OnInit {
         } else {
             var list = document.createElement('ul');
             preview.appendChild(list);
-            for (var i = 0; i < curFiles.length; i++) {
+
+            var fileReader: FileReader = new FileReader();
+            fileReader.onloadend = (e) => {
+                if(e && e.target) {
+                    this.content = (e.target as FileReader).result;    
+                    var binary = "";
+                    var bytes = new Uint8Array(this.content);
+                    var length = bytes.byteLength;
+                    for (var i = 0; i < length; i++) {
+                      binary += String.fromCharCode(bytes[i]);
+                    }
+                    console.log(fileType(bytes));
+                    // call 'xlsx' to read the file
+                    var oFile = XLSX.read(binary, {type: 'binary', cellDates:true, cellStyles:true});
+                    for(let sheetName of oFile.SheetNames) {
+                        var range = XLSX.utils.decode_range(oFile.Sheets[sheetName]['!ref'] as string);
+                        console.log(sheetName);
+                        console.log(range.e);        
+                    }
+                }
+            }
+            fileReader.readAsArrayBuffer(curFiles[0]);
+
                 var listItem = document.createElement('li');
-                if (this.validFileType(curFiles[i])) {
-                    listItem.textContent = 'File name ' + curFiles[i].name + ', file size ' + this.returnFileSize(curFiles[i].size) + '.';
+                if (this.validFileType(curFiles[0])) {
+                    listItem.textContent = 'File name ' + curFiles[0].name + ', file size ' + this.returnFileSize(curFiles[0].size) + '.';
                 } else {
-                    listItem.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type (' + curFiles[i].type + '). Update your selection.';
+                    listItem.textContent = 'File name ' + curFiles[0].name + ': Not a valid file type (' + curFiles[0].type + '). Update your selection.';
                 }
 
                 list.appendChild(listItem);
-            }
+
         }
     };
     validFileType(file: File) {
