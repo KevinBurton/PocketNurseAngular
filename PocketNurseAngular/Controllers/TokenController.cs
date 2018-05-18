7a18c7eb718c99a6039b1ed23857bd14376fb3d2
Ronald Kevin Burton <rkevinburton@charter.net>
2018-05-18 12:41:21 -0500

    Write to tokens to file

--- PocketNurseAngular/Controllers/TokenController.cs
+++ PocketNurseAngular/Controllers/TokenController.cs
@@ -13,30 +13,39 @@ namespace PocketNurseAngular.Controllers
         [HttpPost]
         public void Patient([FromBody]string[] patients)
         {
+            const string filePath = @"Patients.txt";
             if(patients == null || patients.Length < 1 || patients.Any(p => string.IsNullOrWhiteSpace(p)))
             {
+                if (System.IO.File.Exists(filePath)) System.IO.File.Delete(filePath);
                 return;
             }
+            System.IO.File.WriteAllLines(filePath, patients);
             return;
         }
         [Route("MedicationOrder")]
         [HttpPost]
         public void MedicationOrder([FromBody]string[] medorders)
         {
+            const string filePath = @"MedicationOrders.txt";
             if(medorders == null || medorders.Length < 1 || medorders.Any(m => string.IsNullOrWhiteSpace(m)))
             {
+                if (System.IO.File.Exists(filePath)) System.IO.File.Delete(filePath);
                 return;
             }
+            System.IO.File.WriteAllLines(filePath, medorders);
             return;
         }
         [Route("Item")]
         [HttpPost]
         public void Item([FromBody]string[] items)
         {
+            const string filePath = @"Items.txt";
             if(items == null || items.Length < 1 || items.Any(i => string.IsNullOrWhiteSpace(i)))
             {
+                if (System.IO.File.Exists(filePath)) System.IO.File.Delete(filePath);
                 return;
             }
+            System.IO.File.WriteAllLines(filePath, items);
             return;
         }       
     }
